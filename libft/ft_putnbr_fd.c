21e23f97c3b59d3cff7a124d555d4c721137ebe8
Jed Sarkis <jsarkis@c3r3s2.wethinkcode.co.za>
2019-05-23 19:28:01 +0200

    noting specific

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jsarkis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 19:32:22 by jsarkis           #+#    #+#             */
-/*   Updated: 2019/05/22 19:32:24 by jsarkis          ###   ########.fr       */
+/*   Updated: 2019/05/23 19:06:51 by jsarkis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,21 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
-
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-214748364", fd);
+		n = 8;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n < 10)
+		ft_putchar_fd(n + '0', fd);
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd((n % 10) + '0', fd);
+	}
 }
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: jsarkis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 10:36:45 by jsarkis           #+#    #+#             */
-/*   Updated: 2019/05/23 15:29:23 by jsarkis          ###   ########.fr       */
+/*   Updated: 2019/05/23 18:37:09 by jsarkis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 #include <string.h>
 #include <stdlib.h>
 #include "libft.h"
+#include "ft_putnbr_fd.c"
+#include "ft_putchar_fd.c"
+#include "ft_putstr_fd.c"
 
-int main()
+int main(int argc, char *argv[])
 {
 	int i;
 
-	i = 0;
-	return (i);
+	if (argc == 2)
+	{	
+		i = atoi(argv[1]);
+		ft_putnbr_fd(i, 1);
+	}
 }
