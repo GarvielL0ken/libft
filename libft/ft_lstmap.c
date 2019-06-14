fd5cf9011ea114e02d6ec217badb153d38aa6b3c
Jed Sarkis <jsarkis@c3r6s7.wethinkcode.co.za>
2019-06-14 13:50:01 +0200

    fixed ft_lstmap and made it recursive

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jsarkis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 14:13:14 by jsarkis           #+#    #+#             */
-/*   Updated: 2019/05/30 14:13:33 by jsarkis          ###   ########.fr       */
+/*   Updated: 2019/06/14 13:46:03 by jsarkis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*head;
-	t_list	*current;
 
-	head = f(lst);
-	current = head;
-	while (lst)
+	head = NULL;
+	if (lst)
 	{
-		lst = lst->next;
-		current->next = f(lst);
-		current = current->next;
+		head = (t_list *)malloc(sizeof(t_list));
+		if (!head)
+			return (NULL);
+		head = f(lst);
+		head->next = ft_lstmap(lst->next, f);
 	}
 	return (head);
 }
