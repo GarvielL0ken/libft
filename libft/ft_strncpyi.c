d9cef5001c0539309cfeb6012b66aabb06c4751b
Jed Sarkis <jsarkis@c3r6s9.wethinkcode.co.za>
2019-07-02 18:52:16 +0200

    added the opening brace for the function that was missing

--- libft/ft_strncpyi.c
+++ libft/ft_strncpyi.c
@@ -6,12 +6,12 @@
 /*   By: jsarkis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 12:36:16 by jsarkis           #+#    #+#             */
-/*   Updated: 2019/07/02 12:41:01 by jsarkis          ###   ########.fr       */
+/*   Updated: 2019/07/02 18:50:49 by jsarkis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncpyi(char **dst, const char *src, int len, int dst_index)
-
+{
 	int	i;
 
 	i = 0;
