7fc436726eb5350bd5b44b405bf0c90b94ab0675
Jed Sarkis <jsarkis@c2r3s3.wethinkcode.co.za>
2019-10-06 13:10:04 +0200

    changed ft_putstr_fd to print the whole string in one write call

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsarkis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jsarkis <jsarkis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 19:37:40 by jsarkis           #+#    #+#             */
-/*   Updated: 2019/05/30 13:21:16 by jsarkis          ###   ########.fr       */
+/*   Updated: 2019/10/06 13:08:25 by jsarkis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,5 @@ void	ft_putstr_fd(char const *str, int fd)
 {
 	if (!str || !fd)
 		return ;
-	while (*str != '\0')
-	{
-		ft_putchar_fd(*str, fd);
-		str++;
-	}
+	write(fd, str, ft_strlen(str));
 }
